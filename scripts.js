1ef9d68acc96c7113c4e8d550636cdc9098bc279
GabrielFlorea01 <gabriel.florea01@gmail.com>
2024-12-22 14:37:27 +0200

    More files and features added

--- scripts.js
+++ scripts.js
@@ -1,40 +1,4 @@
-function allowDrop(event) {
-    event.preventDefault();
+function togglePromotion() {
+    const promotion = document.getElementById('promotion');
+    promotion.classList.toggle('hidden');
 }
-
-function drag(event) {
-    event.dataTransfer.setData('text', event.target.id);
-}
-
-function drop(event) {
-    event.preventDefault();
-    const data = event.dataTransfer.getData('text');
-    const droppedItem = document.getElementById(data);
-    alert('Produsul ' + droppedItem.querySelector('h3').textContent + ' a fost adăugat în coș!');
-    const basket = document.getElementById('basket');
-    basket.classList.add('droppable');
-    setTimeout(() => {
-        basket.classList.remove('droppable');
-    }, 1000);
-}
-
-document.getElementById('priceRange').addEventListener('input', function () {
-    const selectedPrice = parseInt(this.value);
-    document.getElementById('priceValue').textContent = selectedPrice + " RON";
-    const products = document.querySelectorAll('.produs');
-    products.forEach(product => {
-        const productPrice = parseInt(product.getAttribute('data-price'));
-
-        if (productPrice <= selectedPrice) {
-            product.style.display = 'block';
-        } else {
-            product.style.display = 'none';
-        }
-    });
-});
-
-document.querySelectorAll('.produs button').forEach(button => {
-    button.addEventListener('click', function() {
-        alert('Produsul a fost adăugat în coș!');
-    });
-});
